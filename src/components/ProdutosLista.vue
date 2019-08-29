26b86b4d97ebd6b2dfac90ab57c65a637b79a864
Breno da Silva Lemos <breno@cafazsaude.org.br>
2019-08-29 13:10:21 -0300

    Produtos falta consertar a api

--- src/components/ProdutosLista.vue
+++ src/components/ProdutosLista.vue
@@ -1,7 +1,30 @@
-<template></template>
+<template>
+  <section class="produtos-container">{{produtos}}</section>
+</template>
 
 <script>
-export default {};
+export default {
+  data() {
+    return {
+      produtos: null
+    };
+  },
+  methods: {
+    getProdutos() {
+      fetch({
+        url: "@/api/ranek.json",
+        dataType: "text"
+      })
+        .then(response => response.json())
+        .then(response => {
+          this.produtos = response.produtos;
+        });
+    }
+  },
+  created() {
+    this.getProdutos();
+  }
+};
 </script>
 
 <style>
