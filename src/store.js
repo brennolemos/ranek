22d39a87034aa7e228a248831d918db55e806888
Breno Lemos <brenomiros@gmail.com>
2019-09-14 17:01:17 -0300

    Post Usuário

--- src/store.js
+++ src/store.js
@@ -31,10 +31,16 @@ export default new Vuex.Store({
   },
   actions: {
     getUsuario(context, payload) {
-      api.get(`http://localhost:3000/usuario/${payload}`).then(response => {
-        context.commit("UPDATE_USUARIO", response.data);
-        context.commit("UPDATE_LOGIN", true);
-      });
+      return api
+        .get(`http://localhost:3000/usuario/${payload}`)
+        .then(response => {
+          context.commit("UPDATE_USUARIO", response.data);
+          context.commit("UPDATE_LOGIN", true);
+        });
+    },
+    criarUsuario(context, payload) {
+      context.commit("UPDATE_USUARIO", { id: payload.email });
+      return api.post("http://localhost:3000/usuario", payload);
     }
   }
 });
