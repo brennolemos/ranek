0b32ea8b9a2862e6fbf9d0a8251050b2695796dc
Breno Lemos <brenomiros@gmail.com>
2019-09-15 16:47:23 -0300

    Usuário Produtos

---
+++ src/components/ProdutoAdicionar.vue
@@ -0,0 +1,12 @@
+<template>
+  <h2>Produto Adicionar</h2>
+</template>
+
+<script>
+export default {
+  name: "ProdutoAdicionar"
+};
+</script>
+
+<style>
+</style>
--- src/views/usuario/UsuarioProdutos.vue
+++ src/views/usuario/UsuarioProdutos.vue
@@ -1,9 +1,29 @@
 <template>
-  <p>Lista de Produtos</p>
+  <section>
+    <h2>Adicionar Produto</h2>
+    <ProdutoAdicionar />
+    <h2>Seus Produtos</h2>
+  </section>
 </template>
 
 <script>
-export default {};
+import ProdutoAdicionar from "@/components/ProdutoAdicionar.vue";
+import { mapState, mapActions } from "vuex";
+export default {
+  name: "UsuarioProdutos",
+  components: {
+    ProdutoAdicionar
+  },
+  computed: {
+    ...mapState(["login", "usuario", "usuario_produtos"])
+  },
+  methods: {
+    ...mapActions(["getUsuarioProdutos"])
+  },
+  created() {
+    this.getUsuarioProdutos();
+  }
+};
 </script>
 
 <style>
